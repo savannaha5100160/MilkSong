6e562d2f2d2353475339c88ccd2b95d1b4965c2c
savannaha5100160 <5100160@mypillar7uwc.ac.za>
2023-12-08 10:11:41 +0200

    first commit

---
+++ Assignment1MilkSong/MilkSongcs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1MilkSong
+{
+    internal class MilkSongcs
+    {
+        public void sing()
+        {
+            for (int i = 99; i >= 0; i--)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine("No more bottles of milk on the wall, no more bottles of milk.");
+                    Console.WriteLine("Go to the store and buy some more,99 bottles of milk on the wall.");
+                }
+                else if (i == 1)
+                {
+                    Console.WriteLine("1 bottle of milk on the wall, 1 bottles of milk.");
+                    Console.WriteLine("Take one down and pass it around, no more bottles of milk on the wall.");
+                }
+                else
+                {
+                    Console.WriteLine(i + "bottles of milk on the wall,"  + i + "bottles of milk.");
+                    Console.WriteLine("Take one down and pass it around," + (i - 1) + "bottles of milk on the wall.");
+                }
+                 
+                {
+                    Console.WriteLine(" 1 bottle of milk on the wall," + " 1 bottle of milk.");
+                }
+                
+
+            }
+
+        }
+
+
+
+    }
+}
---
+++ Assignment1MilkSong/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Assignment1MilkSong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MilkSongcs milksong = new MilkSongcs();
+            milksong.sing();
+        }
+    }
+}
